7e273d9d0265c0a2eba19a401dc3d28e29a3d60b
Sarah Kelly <sarahblairkelly@gmail.com>
2020-11-25 16:51:05 +0100

    To facilitate the !timeout. new members are now given a 'member' role. If the role doesn't exist, it's first created. + adding to the quote_list

--- cogs/new_members.py
+++ cogs/new_members.py
@@ -1,3 +1,4 @@
+import discord
 import random
 from discord.ext import commands
 
@@ -6,9 +7,29 @@ class New_Members(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def check_for_role(self, member):
+        guild = member.guild
+        roles = guild.roles
+        name_list = []
+        for each in roles:
+            name_list.append(each.name)
+        if "Member" in name_list:
+            pass
+        else:
+            await self.create_member_role(guild)
+
+    async def create_member_role(self, guild):
+        permissions = discord.Permissions(permissions=104287808)
+        await guild.create_role(name="Member", colour=discord.Colour(0x318ad1), permissions=permissions)
+
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        """sends a welcome message in the channel when a new member joins"""
+        """assigns new member their role and sends a welcome message in the channel when a new member joins"""
+        await self.check_for_role(member)
+        guild = member.guild
+        memberrole = discord.utils.get(guild.roles, name="Member")
+        await member.add_roles(memberrole)
+
         system_channel = member.guild.system_channel
         with open('welcome_message.txt', 'r') as reader:
             welcome_message = random.choice(reader.readlines())
