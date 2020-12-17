3d497214c1d5cfc6300e762f03be18e98884b884
Sarah Kelly <sarahblairkelly@gmail.com>
2020-12-17 17:08:40 +0100

    Only checks if it's someone's birthday if that someone isn't already in the list. Adds the person to the list if they're not

---
+++ birthday_list.py
@@ -0,0 +1,40 @@
+import ast
+import os
+from datetime import date
+
+def get_path(guild):
+    fileDir = os.path.dirname(os.path.abspath(__file__))  # Directory of the Module
+    newPath = os.path.join(fileDir, "GuildDirectories", str(guild.id), "birthday_list.txt")
+    return newPath
+
+def get_dict(guild):
+    """pulls dictionary from text document and turns it into a readable dictionary in programme"""
+    newPath = get_path(guild)
+    with open(newPath, 'r+') as f:
+        data = f.read()
+        fulldict = ast.literal_eval(data) # pulling the text in the document as a dictionary
+        return fulldict
+
+def append_dict(guild, fulldict):
+    """rewrites the dictionary if there are any changes"""
+    newPath = get_path(guild)
+    with open(newPath, 'r+') as f:
+        f.truncate(0)
+        f.seek(0)
+        f.write(str(fulldict))
+
+def add_birthday(guild, member):
+    """adds the birthday to the dictionary with today's date"""
+    bdaymember = member
+    fulldict = get_dict(guild)
+    today = date.today()
+    if bdaymember not in fulldict:
+        fulldict[bdaymember] = today.strftime("%d/%m/%y") # if not, appends them to dictionary and sets their number as 1
+    append_dict(guild, fulldict) # rewrites the dictionary to the file
+
+def check_birthday(guild, member):
+    fulldict = get_dict(guild)
+    if member in fulldict:
+        return True
+    else:
+        return False
--- cogs/birthday_reader.py
+++ cogs/birthday_reader.py
@@ -18,22 +18,27 @@ class Birthday(commands.Cog):
         ids = self.bot.user.id
         if "happy birthday" in new_message:
             user = message.mentions[0] # pulls the first user mention. Will break if there is more than one
-            if message.mentions == []:
+            member = '#'.join((user.name, user.discriminator))
+            if birthday_list.check_birthday(message.guild, member) == True:
                 pass
             else:
-                await channel.send("Wait, is it {0}'s birthday?!".format(user.mention))
+                if message.mentions == []:
+                    pass
+                else:
+                    await channel.send("Wait, is it {0}'s birthday?!".format(user.mention))
 
-            def check(message):
-                cleanmessage = cleanup.easyreader(message)
-                return message.channel == channel and str(ids) in cleanmessage and "yes" in cleanmessage
-                # The about number is pulled as the bot's client id. Should work across all servers.
+                def check(message):
+                    cleanmessage = cleanup.easyreader(message)
+                    return message.channel == channel and str(ids) in cleanmessage and "yes" in cleanmessage
+                    # The about number is pulled as the bot's client id. Should work across all servers.
 
-            try:
-                await self.bot.wait_for('message', timeout=60.0, check=check) # waits for response for 1 min
-            except asyncio.TimeoutError:
-                await channel.send("Oh, I guess not.") # if it doesn't get the right response in time
-            else:
-                await channel.send("Why didn't anyone tell me?! HAVE THE BEST OF BIRTHDAYS {0}!!".format(user.mention))
+                try:
+                    await self.bot.wait_for('message', timeout=60.0, check=check) # waits for response for 1 min
+                except asyncio.TimeoutError:
+                    await channel.send("Oh, I guess not.") # if it doesn't get the right response in time
+                else:
+                    await channel.send("Why didn't anyone tell me?! HAVE THE BEST OF BIRTHDAYS {0}!!".format(user.mention))
+                    birthday_list.add_birthday(message.guild, member)
 
 
 def setup(bot):
